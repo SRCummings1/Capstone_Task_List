71d960c38a236bcd8808dabbfe0be39c9808962a
Sean <srcummings85@gmail.com>
2020-01-31 08:49:20 -0500

    first commit starting repo

---
+++ Capstone_Task_List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Capstone_Task_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
